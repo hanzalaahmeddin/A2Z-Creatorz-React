e6d4de50059a51ab6ca39fb795b2ee11f76e2a59
hanzalaahmeddin <hanzala.ahmed.promos@gmail.com>
2026-01-15 14:37:16 +0500

    service image fixed

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -4,13 +4,13 @@ import ServicesComponent from "../components/home/Services";
 import ContactReachUs from "../components/ContactReachUs";
 
 function Services() {
-    usePageTitle('Our Services - A2Z Creatorz | Web Design, Mobile Apps, Digital Marketing');
+  usePageTitle('Our Services - A2Z Creatorz | Web Design, Mobile Apps, Digital Marketing');
   return (
     <div>
       <InnerPageBanner
         title="Our Services"
         subtitle="Explore our comprehensive range of digital services designed to elevate your business."
-        imageSrc="/src/assets/images/service-right-image.png"
+        imageSrc="/assets/images/service-right-image.png"
       />
       <ServicesComponent />
       <ContactReachUs />
